7659256348370304da7536cdd44952f863b706c1
oleksii.velychko <oleksii.v.velychko@gmail.com>
2018-11-01 16:50:44 +0200

    add tests

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # django-bank
-Online banking
+This project demonstrates simple solution based on online banking theme.
+Application provides secure form to manage operations over and between credit cards. 
+Included Redis storage for client cards.
+Also included Adminer (docker container) for manage database.
 
 Please visit <a href="https://bank.alexdevel.pro">https://bank.alexdevel.pro</a> (production version)
 
@@ -9,4 +12,65 @@ Test user:
 > password: djangodjango
 
 
+Test user credit card:
+> Card number 9027578398661306
+
+> Pincode 1366
+
 ![alt text](https://raw.githubusercontent.com/oleksii-velychko/django-bank/master/screenshot.png)
+
+
+In order to start this project (after cloning) need to follow next steps:
+
+1. To ensure HTTPS connections need a generate self-signed certificate:
+
+    ```
+    cd django-bank/docker/openssl
+    mkdir certs
+
+    openssl genrsa -des3 -out certs/rootCA.key 2048
+    openssl req -x509 -new -nodes -key certs/rootCA.key -sha256 -days 365 -out certs/rootCA.pem
+    openssl req -new -sha256 -nodes -out certs/server.csr -newkey rsa:2048 -keyout certs/server.key -config <( cat server.csr.cnf )
+    openssl x509 -req -in certs/server.csr -CA certs/rootCA.pem -CAkey certs/rootCA.key -CAcreateserial -out certs/server.crt -days 500 -sha256 -extfile v3.ext
+    ```
+
+    Then import rootCA.pem to browser through Settings -> Manage Certficates -> Authorities tabs
+
+2. Build and run dockker containers:
+
+    ```
+    docker-compose build
+    docker-compose up -d
+    docker-compose ps
+    ```
+    
+    last command will show status of running containers.
+    
+3. Add services names to /etc/hosts
+
+    ```
+    # VirtualHosts
+    127.0.0.1       app.local postgres redis
+    ```
+    
+4. Now install and js libraries:
+
+    ```
+    cd django/project/staticfiles
+    npm install
+    ```
+    
+5. Execute Django commands:
+
+    ```
+    cd django
+    python3 ./manage.py makemigrations --settings=project.local
+    python3 ./manage.py migrate --settings=project.local
+    python3 ./manage.py collectstatic
+    ```
+    
+6. Run tests for banking application:
+
+    ```
+    python3 ./manage.py test banking
+    ```
--- django/project/apps/banking/tests.py
+++ django/project/apps/banking/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from django.conf import settings
 from project.apps.banking.models import Card, Transaction
+from project.apps.banking.forms import TransactionForm
 
 
 class BankingTestCase(TestCase):
@@ -11,4 +12,119 @@ class BankingTestCase(TestCase):
         :return:
         """
         self.user = User.objects.create_user(username='testuser', password='12345')
-        login = self.client.login(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+        self.card = Card.objects.create(user=self.user)
+
+    def test_auth_home_path(self):
+        """
+        :return:
+        """
+        print('\nRunning test [test_home_path_return_200]')
+
+        self.response = self.client.get('/banking/')
+        print('Response status code:', self.response.status_code)
+
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_modal_create_credit_card(self):
+        """
+        :return:
+        """
+        print('\nRunning test [test_create_credit_card]')
+
+        cards_count_before = Card.objects.count()
+        print(f'Cards count before: {cards_count_before}')
+
+        card_create_request_url = '/banking/create/'
+        print('Card request AJAX URL:', card_create_request_url)
+
+        result = self.client.post(card_create_request_url, {
+            'user': self.user,
+        }, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+
+        print(result)
+
+        cards_count_after = Card.objects.count()
+        print(f'Cards count after: {cards_count_after}')
+
+        self.assertEqual(result.status_code, 200)
+        self.assertGreater(cards_count_after, cards_count_before)
+
+    def test_put_money_to_card(self):
+        """
+        :return:
+        """
+        print('\nRunning test [test_put_money_to_card]')
+
+        print(f'\nBalance before transaction: {self.card.balance}')
+
+        form_data = {
+            'card_from': self.card.id,
+            'card_to': None,
+            'sum': 100,
+            'pin': self.card.pin,
+            'operation': 'plus'
+        }
+
+        form = TransactionForm(self.user.id, form_data)
+        self.assertTrue(form.is_valid())
+        form.save()
+
+        self.card.refresh_from_db()
+        print(f'\nBalance after transaction: {self.card.balance}')
+        self.assertGreater(self.card.balance, 0)
+
+    def test_take_money_from_card(self):
+        """
+        :return:
+        """
+        print('\nRunning test [test_take_money_from_card]')
+
+        form_data = {
+            'card_from': self.card.id,
+            'card_to': None,
+            'sum': 200,
+            'pin': self.card.pin,
+            'operation': 'minus'
+        }
+
+        form = TransactionForm(self.user.id, form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_transfer_money_to_card(self):
+        """
+        :return:
+        """
+        print('\nRunning test [test_transfer_money_to_card]')
+
+        form_data = {
+            'card_from': self.card.id,
+            'card_to': None,
+            'sum': 100,
+            'pin': self.card.pin,
+            'operation': 'plus'
+        }
+
+        form = TransactionForm(self.user.id, form_data)
+        self.assertTrue(form.is_valid())
+        form.save()
+
+        card_receiver = Card.objects.create(user=self.user)
+
+        print(f'\nBalance before transaction: {card_receiver.balance}')
+
+        form_data = {
+            'card_from': self.card.id,
+            'card_to': card_receiver.id,
+            'sum': 10,
+            'pin': self.card.pin,
+            'operation': 'transfer'
+        }
+
+        form = TransactionForm(self.user.id, form_data)
+        self.assertTrue(form.is_valid())
+        form.save()
+
+        card_receiver.refresh_from_db()
+        print(f'\nBalance after transaction: {card_receiver.balance}')
+        self.assertGreater(card_receiver.balance, 0)
